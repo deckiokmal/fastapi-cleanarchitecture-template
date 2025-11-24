f885180725b5c7f9bcc986d90bab4d07b036497d
Deckiokmal <dopnetindo@gmail.com>
2025-11-25 01:55:59 +0700

    CORS Middleware

--- src/main.py
+++ src/main.py
@@ -4,15 +4,39 @@ from .entities.todo import Todo  # Import models to register them
 from .entities.user import User  # Import models to register them
 from .api import register_routes
 from .logging import configure_logging, LogLevels
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 configure_logging(LogLevels.info)
 
 app = FastAPI()
 
+# Get environment
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+
+if ENVIRONMENT == "development":
+    allow_origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+else:
+    allow_origins = [
+        "https://yourdomain.com",
+        "https://www.yourdomain.com",
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 """ Only uncomment below to create new tables, 
 otherwise the tests will fail if not connected
 """
-create_tables()
+# create_tables()
 
 register_routes(app)
